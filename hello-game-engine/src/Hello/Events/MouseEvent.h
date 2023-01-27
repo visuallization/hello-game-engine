20fad6e868fea65cc291b2e7c9679a281d8afbde
visuallisation <florentin.rieger@gmail.com>
2023-01-27 13:36:26 +0100

    engine: fix(mouse move): fix mouse move event by returning the correct value

--- hello-game-engine/src/Hello/Events/MouseEvent.h
+++ hello-game-engine/src/Hello/Events/MouseEvent.h
@@ -9,7 +9,7 @@ namespace Hello {
 		MouseMovedEvent(float x, float y): m_MouseX(x), m_MouseY(y) {}
 
 		inline float GetX() const { return m_MouseX; }
-		inline float GetY() const { return m_MouseX; }
+		inline float GetY() const { return m_MouseY; }
 
 		std::string ToString() const override {
 			std::stringstream ss;

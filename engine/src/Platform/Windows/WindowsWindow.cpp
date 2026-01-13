9748b2670c969c7c123275915428296c650770ff
Florentin <florentin@Florentins-MacBook-Air.local>
2026-01-13 23:39:05 +0100

    feat(mac): added compile_commands.json to support intellisense on cursor

--- engine/src/Platform/Windows/WindowsWindow.cpp
+++ engine/src/Platform/Windows/WindowsWindow.cpp
@@ -42,12 +42,10 @@ namespace Hello {
 		}
 
 		// Set OpenGL context hints before creating window
-		#ifdef HO_PLATFORM_MAC
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-			glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
-			glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-			glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-		#endif
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
 		m_Window = glfwCreateWindow(props.With, props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 		glfwMakeContextCurrent(m_Window);
